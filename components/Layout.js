79fae53a9bbb73e8e89e9f7c699ff99b5bbd6c60
Matt G <mgipson@gmail.com>
2025-01-19 15:59:35 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
 
         <main>{children}</main>
         <footer style={{ color: '#666', padding: '1rem', fontSize: '14px', textAlign: 'center' }}>
-        <p><strong>Audiotron3000</strong> / A React soundboard app framework by <a href="https://msg317.com" target="_blank">MSG317</a>.</p>
+        <p><strong><a href="https://github.com/mattgipson/soundboard-msg317" target="_blank">Audiotron3000</a></strong> / A React soundboard app framework by <a href="https://msg317.com" target="_blank">MSG317</a>.</p>
         <br/>
         <p>
           <strong>How to use:</strong><br />
